25e4d591b7b4f98f79aa2595eb56296a7b3d513b
Konrad Rudolph <konrad_rudolph@madrat.net>
2012-09-14 15:17:09 +0100

    Removed const qualifiers

--- test.cpp
+++ test.cpp
@@ -6,20 +6,20 @@ using std::cout;
 using util::lang::range;
 
 int main() {
-    for (auto const i : range(1, 5))
+    for (auto i : range(1, 5))
         cout << i << "\n";
 
-    for (auto const u : range(0u))
+    for (auto u : range(0u))
         if (u == 3u) break;
         else         cout << u << "\n";
 
-    for (auto const c : range('a', 'd'))
+    for (auto c : range('a', 'd'))
         cout << c << "\n";
 
-    for (auto const u : range(20u, 29u).step(2u))
+    for (auto u : range(20u, 29u).step(2u))
         cout << u << "\n";
 
-    for (auto const i : range(100).step(-3))
+    for (auto i : range(100).step(-3))
         if (i < 90) break;
         else        cout << i << "\n";
 }
