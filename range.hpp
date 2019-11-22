65ab2de9b9167383e4a29f6bca751efe1500a660
Arnaud Barré <alzathar@Moveck-C05.local>
2019-11-21 23:05:15 -0500

    fix a compilation error in VS2019
    'not' keyword unknown.

--- range.hpp
+++ range.hpp
@@ -32,7 +32,7 @@ struct range_iter_base : std::iterator<std::input_iterator_tag, T> {
     }
 
     bool operator !=(range_iter_base const& other) const {
-        return not (*this == other);
+        return !(*this == other);
     }
 
 protected:
@@ -72,7 +72,7 @@ struct range_proxy {
             }
 
             bool operator !=(iter const& other) const {
-                return not (*this == other);
+                return !(*this == other);
             }
 
         private:
